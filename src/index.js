75fcf0d6b275ac63e1f7518fb979645b41692235
Zac Edens <edens.zac@gmail.com>
2021-11-29 10:35:32 -0800

    updated our URI to be our deployed DB

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import GlobalStyles from "./styles";
 import Pages from "./pages";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  uri: "https://apollo-odyssey-server-edens.herokuapp.com/",
   cache: new InMemoryCache(),
 });
 
